85c0e244f798b5c33575a7e04051d080dc28caaa
kobalt42z <chmouel2690@gmail.com>
2022-08-16 20:32:40 +0300

    last V of targil 1

--- targil1.c
+++ targil1.c
@@ -1,17 +1,42 @@
 #include <stdio.h>
 #include <math.h>
 
-float avrage(int n);
+float avrage_smallerThanFirst(int n);
+float avrage_smallerThanLast(int n);
 int main(int argc, char const *argv[])
 {
-    int n;
-    printf("please enter a number od iteration \n");
-    printf("==>");
-    scanf("%d", &n);
-    printf("the avrage is %f", avrage(n));
+    int n, choice;
+    printf("please choose a number betwen the options below\n\n");
+    printf("1 ==> avrage_smallerThanFirst \n");
+    printf("2 ==> avrage_smallerThanLast\n");
+    printf("--> ");
+    scanf("%d", &choice);
+
+    switch (choice)
+    {
+    case 1:
+        printf("\n you chosse : 1 ==> avrage_smallerThanFirst \n");
+        printf("please enter a number of iteration \n");
+        printf("==>");
+        scanf("%d", &n);
+        printf("the avrage_smallerThanFirst is %0.3f", avrage_smallerThanFirst(n));
+        break;
+    case 2:
+        printf(" \n you chosse : 2 ==> avrage_smallerThanLast \n");
+        printf("please enter a number of iteration \n");
+        printf("==>");
+        scanf("%d", &n);
+        printf("the avrage_smallerThanFirst is %0.3f", avrage_smallerThanLast(n));
+        break;
+
+    default:
+        printf("invalid choice \n");
+        break;
+    }
+
     return 0;
 }
-float avrage(int n)
+float avrage_smallerThanFirst(int n)
 {
 
     int a[n];
@@ -32,5 +57,29 @@ float avrage(int n)
         }
     }
     res = res / div;
-    return res;  
+    return res;
+}
+
+float avrage_smallerThanLast(int n)
+{
+
+    int a[n];
+    float res, div;
+    res = div = 0.0;
+    for (size_t i = 0; i < n; i++)
+    {
+        printf("please enter a number  \n");
+        scanf("%d", &a[i]);
+    }
+
+    for (size_t i = 0; i < n; i++)
+    {
+        if (a[i] < a[n-1])
+        {
+            res += a[i];
+            div++;
+        }
+    }
+    res = res / div;
+    return res;
 }
---
+++ targil2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(int argc, char const *argv[])
+{
+    int AA[2] = {1, 2,3};
+    printf("hello world\n");
+    printf("%d\n", AA[2]);
+    return 0;
+}
