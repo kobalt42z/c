a987041024b596fcbe3b00ff1a1bc9b1df2e42c2
kobalt42z <chmouel2690@gmail.com>
2022-08-17 12:51:09 +0300

    final version of targil 2

--- targil2.c
+++ targil2.c
@@ -21,13 +21,8 @@ int main(int argc, char const *argv[])
 
 void tasko(int n)
 {
-    int arrOfCounters[9]; // ?array of 0-9 counters //++ on if found in arr_a
-
-    //! array reset to  0 //  avoiding fals val if not found in arr_a!
-    for (size_t i = 0; i < 8; i++)
-    {
-        arrOfCounters[i] = 0;
-    }
+    int numInArr = 9;
+    int temp = 0;
 
     //! this is a calaulus formula that returns dig count for a better use of memory
     int nDigits = floor(log10(abs(n))) + 1;
@@ -35,36 +30,30 @@ void tasko(int n)
 
     //* task 1
     //? split num to array of digits
-    for (size_t i = 0; i < nDigits - 1; i++)
+    for (size_t i = 0; i < nDigits ; i++)
     {
         arr_a[i] = n % 10;
         n /= 10;
     }
 
-    //* task 2
-    int i, j;
-    i = j = 0;
-    while (i < 8)
+    while (numInArr != 0)
     {
-        while (j < nDigits - 1)
-        {
-            if (arr_a[j] == arrOfCounters[i])
-            {
-                arrOfCounters[i]++;
-            }
-            j++;
-        }
-        i++;
-    }
 
-    for (int i = 0; i < 8; i++)
-    {
-        if (arrOfCounters[i] != 0)
+        for (size_t ii = 0; ii < nDigits ; ii++)
         {
-            printf(" the amount of %d \t %d", i+1, arrOfCounters[i]);
+            if (arr_a[ii] == numInArr)
+            {
+                temp++;
+            }
         }
+
+        if (temp != 0)
+        {
+
+            printf("the amount of %d in number is : %d\n", numInArr, temp);
+        }
+        numInArr--;
+        temp = 0;
     }
 }
 
-
-//! bad algo we can do it much faster
