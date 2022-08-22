878efe12ba5e35856139a7d8a5755b25a87679db
kobalt42z <chmouel2690@gmail.com>
2022-08-22 20:57:59 +0300

    new4

--- targil4.c
+++ targil4.c
@@ -2,29 +2,54 @@
 #include <math.h>
 #include <stdlib.h>
 
-int countSeries(int n);
+void countSeries(long int n);
 int main(int argc, char const *argv[])
 {
+    // ? input here 
+    countSeries(111101111011);
 
     return 0;
 }
 
-int countSeries(int n)
+void countSeries(long int n)
 {
-int index,reset,seriesAmount[12];
+    int index = 0, reset, seriesAmount[12], temp;
 
-for (size_t i = 0; i < 11 ; i++)
-{
-    switch (seriesAmount[i])
+    for (size_t i = 0; i < 12; i++)
     {
-    case 0:
-        /* code */
-        break;
-    
-    default:
-        
-        break;
+        seriesAmount[i] = 0;
     }
-}
 
+    while (n > 0)
+    {
+
+        temp = n % 10;
+
+        switch (temp)
+        {
+        case 0:
+            seriesAmount[index - 1]++;
+            index = 0;
+            break;
+
+        case 1:
+            index++;
+            break;
+
+        default:
+            printf("invalid\n");
+            return 0;
+        }
+        n /= 10;
+    }
+    seriesAmount[index - 1]++;
+
+    for (size_t i = 0; i < 12; i++)
+    {
+        if (seriesAmount[i] != 0)
+        {
+               printf("there are %d  series of length %ld\n", seriesAmount[i],i+1);
+        }
+        
+    }
 }
